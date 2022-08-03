1cce071e3c96aee7e54639b8812efd9e60db8bcb
Mathieu Barisaux <mathieu.barisaux@gmail.com>
2022-08-03 13:55:24 +0200

    Test for netlify

--- src/containers/UserManagement/UserManagement.js
+++ src/containers/UserManagement/UserManagement.js
@@ -7,19 +7,10 @@ import { useNavigate } from "react-router-dom";
 import robot from "../../assets/img/Saly-26-min.webp";
 
 // ** Components **
-import SignUp from "../../components/SignUp/SignUp";
-import SignIn from "../../components/SignIn/SignIn";
 import { useEffect } from "react";
 
 const UserManagement = (props) => {
-  const {
-    type,
-    bearerToken,
-    tokenChange,
-    setTokenChange,
-    userInformationsChange,
-    setUserInformationsChange,
-  } = props;
+  const { type, bearerToken, tokenChange } = props;
 
   const naviguate = useNavigate();
 
@@ -50,7 +41,7 @@ const UserManagement = (props) => {
           <p>Flark</p>
         </div>
 
-        {type === "signup" ? (
+        {/* {type === "signup" ? (
           <SignUp
             tokenChange={tokenChange}
             setTokenChange={setTokenChange}
@@ -64,7 +55,7 @@ const UserManagement = (props) => {
             userInformationsChange={userInformationsChange}
             setUserInformationsChange={setUserInformationsChange}
           />
-        )}
+        )} */}
 
         <p className="UserManagement__copyright">© Flark 2022</p>
       </div>
